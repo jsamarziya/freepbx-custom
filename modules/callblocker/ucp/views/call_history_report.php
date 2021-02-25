239aa208803248dddc375b00e50c7c548038e946
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2021-02-25 09:52:06 -0800

    added divider

--- modules/callblocker/ucp/views/call_history_report.php
+++ modules/callblocker/ucp/views/call_history_report.php
@@ -1,8 +1,14 @@
 <div class="col-md-12">
     <select data-toggle="select" data-size="auto" title="Date Range">
         <option>All</option>
+        <option data-divider="true"></option>
         <option>2021</option>
         <option>2020</option>
     </select>
 </div>
 
+<select>
+    $(document).on("post-body.widgets", function(event, dashboard_id) {
+    alert("load");
+    });
+</select>
