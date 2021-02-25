418bf9a3565abe50daa8f6e113a71fceafbd52a3
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2021-02-25 09:42:59 -0800

    added select title

--- modules/callblocker/ucp/views/call_history_report.php
+++ modules/callblocker/ucp/views/call_history_report.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">
-    <select data-toggle="select" data-size="auto">
+    <select data-toggle="select" data-size="auto" title="Date Range">
         <option>All</option>
         <option>2021</option>
         <option>2020</option>
