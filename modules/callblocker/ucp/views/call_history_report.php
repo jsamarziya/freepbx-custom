93e714bf7c62ccc2571f9b94d6af767fc2905198
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2021-02-25 11:39:59 -0800

    add styling

--- modules/callblocker/ucp/views/call_history_report.php
+++ modules/callblocker/ucp/views/call_history_report.php
@@ -1,9 +1,9 @@
 <div class="col-md-12">
     <select id="call-history-report-date" data-toggle="select" data-size="auto" title="Date Range">
     </select>
-    <h3>
-        <span id="calls-accepted" class="label label-success"></span>
-        <span id="calls-blocked" class="label label-warning"></span>
+    <h3 style="margin-top: 1em;">
+        <span id="calls-accepted" class="label label-success" style="margin-right: 5ex;"></span>
+        <span id="calls-blocked" class="label label-warning" style="margin-right: 5ex;"></span>
         <span id="calls-blacklisted" class="label label-danger"></span>
     </h3>
 </div>
