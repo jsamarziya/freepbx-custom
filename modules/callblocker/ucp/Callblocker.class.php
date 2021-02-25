e025422491eb98bf2a2183f735cfe0495c38df0d
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2021-02-25 13:52:51 -0800

    fixed reference

--- modules/callblocker/ucp/Callblocker.class.php
+++ modules/callblocker/ucp/Callblocker.class.php
@@ -552,7 +552,7 @@ EOT;
             $year_record = null;
             foreach ($year_records as &$record) {
                 if ($record['cid'] == $call['cid'] and $record['disposition'] == $call['disposition']) {
-                    $year_record = $record;
+                    $year_record = &$record;
                     break;
                 }
             }
@@ -563,10 +563,7 @@ EOT;
                 $call['description'] = [$description];
                 $report[$year][] = $call;
             } else {
-                dbug("updating existing record");
-                dbug($year_record);
                 $year_record['description'][] = $description;
-                dbug($year_record);
             }
         }
         return $report;
