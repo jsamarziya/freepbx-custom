d36156aa271769f8690df5e08b83b33e08fe3db7
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2021-02-25 16:18:33 -0800

    adjusted layout

--- modules/callblocker/ucp/views/call_history_report.php
+++ modules/callblocker/ucp/views/call_history_report.php
@@ -6,30 +6,32 @@
         <span id="calls-blacklisted" class="label label-danger" style="margin-left: 5ex;"></span>
         <span id="calls-accepted" class="label label-success" style="margin-left: 5ex;"></span>
     </h3>
-    <div id="blocked-callers-table-toolbar">
-        <h4 style="margin-top: 2em;">Blocked Calls</h4>
+    <div style="margin-top: 1em;">
+        <div id="blocked-callers-table-toolbar">
+            <h4 style="margin-top: 2em;">Blocked Calls</h4>
+        </div>
+        <table id="blocked-callers-table"
+               class="table"
+               data-toolbar="#blocked-callers-table-toolbar"
+               data-cookie="true"
+               data-cookie-id-table="ucp-callblocker-blocked-callers-table"
+               data-show-toggle="true"
+               data-toggle="table"
+               data-pagination="true"
+               data-search="true"
+               data-sort-name="cid"
+               data-escape="true"
+        >
+            <thead>
+            <tr>
+                <th data-field="count" data-sortable="true">Calls</th>
+                <th data-field="cid" data-sortable="true">CID</th>
+                <th data-field="description" data-sortable="true"
+                    data-formatter="UCP.Modules.Callblocker.formatCallerDescription">
+                    Description
+                </th>
+            </tr>
+            </thead>
+        </table>
     </div>
-    <table id="blocked-callers-table"
-           class="table"
-           data-toolbar="#blocked-callers-table-toolbar"
-           data-cookie="true"
-           data-cookie-id-table="ucp-callblocker-blocked-callers-table"
-           data-show-toggle="true"
-           data-toggle="table"
-           data-pagination="true"
-           data-search="true"
-           data-sort-name="cid"
-           data-escape="true"
-    >
-        <thead>
-        <tr>
-            <th data-field="count" data-sortable="true">Calls</th>
-            <th data-field="cid" data-sortable="true">CID</th>
-            <th data-field="description" data-sortable="true"
-                data-formatter="UCP.Modules.Callblocker.formatCallerDescription">
-                Description
-            </th>
-        </tr>
-        </thead>
-    </table>
 </div>
