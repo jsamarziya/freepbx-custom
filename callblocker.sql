a160a79e7b7d6a7c93e3313bf595479a0143c0c6
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2022-01-31 19:50:30 -0800

    added settings table

--- callblocker.sql
+++ callblocker.sql
@@ -15,3 +15,11 @@ CREATE TABLE `whitelist` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_whitelist_cid_number` (`cid_number`)
 );
+
+CREATE TABLE `settings` (
+  `id`    INT(11)     NOT NULL AUTO_INCREMENT,
+  `name`  VARCHAR(20) NOT NULL,
+  `value` VARCHAR(100)         DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_settings_name` (`name`)
+);
