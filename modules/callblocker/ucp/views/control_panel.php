2af7e90b3862f700f9a9c7e53fd368911e869d52
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2022-01-31 18:27:47 -0800

    rearranged elements

--- modules/callblocker/ucp/views/control_panel.php
+++ modules/callblocker/ucp/views/control_panel.php
@@ -1,24 +1,28 @@
 <div class="col-md-12">
-    Call Blocker&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input type="checkbox" id="cbenable" name="cbenable" data-toggle="toggle" data-on="Enabled" data-off="Disabled" data-size="mini">
-    <script>
-        function getCallBlockerStatus() {
-            $.ajax({
-                url: "ajax.php",
-                data: {
-                    "module": "callblocker",
-                    "command": "getCallBlockerStatus",
-                },
-                success: function (data) {
-                    updateCallBlockerEnabled(data);
-                }
-            });
-        }
-
-        function updateCallBlockerEnabled(data) {
-            $('#cbenable').bootstrapToggle(data["enabled"] ? 'on' : 'off');
-        }
-
-        getCallBlockerStatus();
-    </script>
+    <div>
+        Call Blocker&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="checkbox" id="cbenable" name="cbenable" data-toggle="toggle" data-on="Enabled" data-off="Disabled"
+               data-size="mini">
+    </div>
 </div>
+
+<script>
+    function getCallBlockerStatus() {
+        $.ajax({
+            url: "ajax.php",
+            data: {
+                "module": "callblocker",
+                "command": "getCallBlockerStatus",
+            },
+            success: function (data) {
+                updateCallBlockerEnabled(data);
+            }
+        });
+    }
+
+    function updateCallBlockerEnabled(data) {
+        $('#cbenable').bootstrapToggle(data["enabled"] ? 'on' : 'off');
+    }
+
+    getCallBlockerStatus();
+</script>
