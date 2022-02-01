72befa2cabacd8ab552fdd3b2019209ff143c42e
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2022-01-31 17:27:24 -0800

    added elements

--- modules/callblocker/ucp/views/control_panel.php
+++ modules/callblocker/ucp/views/control_panel.php
@@ -1,3 +1,10 @@
 <div class="col-md-12">
-:)
+    Call Blocker
+    <div class="onoffswitch">
+        <input type="checkbox" name="enabled" class="onoffswitch-checkbox" id="enabled">
+        <label class="onoffswitch-label" for="enabled">
+            <div class="onoffswitch-inner"></div>
+            <div class="onoffswitch-switch"></div>
+        </label>
+    </div>
 </div>
