5c37f91b098450900ba527aeac6d38b9b980aca5
Jeffrey Samarziya <jsamarziya@users.noreply.github.com>
2022-01-31 17:32:46 -0800

    adjusted markup

--- modules/callblocker/ucp/views/control_panel.php
+++ modules/callblocker/ucp/views/control_panel.php
@@ -1,16 +1,18 @@
 <div class="col-md-12">
-    <tr>
-        <td>
-            Call Blocker
-        </td>
-        <td>
-            <div class="onoffswitch">
-                <input type="checkbox" name="enabled" class="onoffswitch-checkbox" id="enabled">
-                <label class="onoffswitch-label" for="enabled">
-                    <div class="onoffswitch-inner"></div>
-                    <div class="onoffswitch-switch"></div>
-                </label>
-            </div>
-        </td>
-    </tr>
+    <table>
+        <tr>
+            <td>
+                Call Blocker
+            </td>
+            <td>
+                <div class="onoffswitch">
+                    <input type="checkbox" name="enabled" class="onoffswitch-checkbox" id="enabled">
+                    <label class="onoffswitch-label" for="enabled">
+                        <div class="onoffswitch-inner"></div>
+                        <div class="onoffswitch-switch"></div>
+                    </label>
+                </div>
+            </td>
+        </tr>
+    </table>
 </div>
